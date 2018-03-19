90b07dd61f3fb18fcf54d5b1e0b7e13717892ee1
Anton Maminov <anton.linux@gmail.com>
2018-03-19 14:29:39 +0200

    allow to use Redis with a custom configuration

--- src/cache/stores/redis_store.cr
+++ src/cache/stores/redis_store.cr
@@ -4,17 +4,27 @@ require "redis"
 module Cache
   # A cache store implementation which stores data in Redis.
   #
-  # By default address is equal localhost:6379
-  #
   # ```crystal
   # cache = Cache::RedisStore(String, String).new(expires_in: 1.minute)
   # cache.fetch("today") do
   #   Time.now.day_of_week
   # end
   # ```
+  #
+  # This assumes Redis was started with a default configuration, and is listening on localhost, port 6379.
+  #
+  # You can connect to `Redis` by instantiating the `Redis` class.
+  #
+  # If you need to connect to a remote server or a different port, try:
+  #
+  # ```crystal
+  # redis = Redis.new(host: "10.0.1.1", port: 6380, password: "my-secret-pw", database: "my-database")
+  # cache = Cache::RedisStore(String, String).new(expires_in: 1.minute, cache: redis)
+  # ```
   struct RedisStore(K, V) < Store(K, V)
-    def initialize(@expires_in : Time::Span)
-      @cache = Redis.new
+    @cache : Redis
+
+    def initialize(@expires_in : Time::Span, @cache = Redis.new)
     end
 
     def write(key : K, value : V, *, expires_in = @expires_in)
