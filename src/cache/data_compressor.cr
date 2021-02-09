de0f5ee02d822b99662f8e425c72fa8623b757fa
Anton Maminov <anton.linux@gmail.com>
2021-02-09 10:41:48 +0200

    Use short block notation

--- src/cache/data_compressor.cr
+++ src/cache/data_compressor.cr
@@ -7,9 +7,7 @@ module Cache
     def deflate(data : String) : String
       io = IO::Memory.new
 
-      Compress::Zlib::Writer.open(io) do |writer|
-        writer.print(data)
-      end
+      Compress::Zlib::Writer.open(io, &.print(data))
 
       # base64-encode the compressed data to make it printable
       Base64.encode(io.to_s)
@@ -20,9 +18,7 @@ module Cache
 
       io = IO::Memory.new(encoded_data.to_slice)
 
-      Compress::Zlib::Reader.open(io) do |reader|
-        reader.gets_to_end
-      end
+      Compress::Zlib::Reader.open(io, &.gets_to_end)
     end
   end
 end
